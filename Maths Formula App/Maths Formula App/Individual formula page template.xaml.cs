57beace73634d4b2ef7174bd7d3298e4dab9dea6
Matthieu Cobby Gagnon <1020709@student.ccgs.wa.edu.au>
2019-07-31 16:07:56 +0800

    added a template for the individual formalae

---
+++ Maths Formula App/Maths Formula App/Individual formula page template.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Maths_Formula_App
+{
+    /// <summary>
+    /// Interaction logic for Individual_formula_page_template.xaml
+    /// </summary>
+    public partial class Individual_formula_page_template : Page
+    {
+        public Individual_formula_page_template()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
