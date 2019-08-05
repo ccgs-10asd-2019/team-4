4ca33d31cf983f2d9695f859d6739b71126bf87c
Matthieu Cobby Gagnon <1020709@student.ccgs.wa.edu.au>
2019-08-05 10:41:26 +0800

    updated formula page template.

--- Maths Formula App/Maths Formula App/Individual_formula_page_template.xaml.cs
+++ Maths Formula App/Maths Formula App/Individual_formula_page_template.xaml.cs
@@ -24,5 +24,10 @@ namespace Maths_Formula_App
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
