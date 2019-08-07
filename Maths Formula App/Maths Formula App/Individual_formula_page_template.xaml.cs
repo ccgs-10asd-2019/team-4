5c20dc67574f1c51f84e936c0a20e2305198582e
Matthieu Cobby Gagnon <1020709@student.ccgs.wa.edu.au>
2019-08-07 09:29:41 +0800

    dicided not to make a drawing slate.

--- Maths Formula App/Maths Formula App/Individual_formula_page_template.xaml.cs
+++ Maths Formula App/Maths Formula App/Individual_formula_page_template.xaml.cs
@@ -25,9 +25,33 @@ namespace Maths_Formula_App
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
 
+
+        /* APPARENTLY SOLVES THE QUADRATIC EQUATION. 
+        public static void SolveQuadratic(double a, double b, double c, out double x1, out double x2)
+        {
+            //Quadratic Formula: x = (-b +- sqrt(b^2 - 4ac)) / 2a
+
+            //Calculate the inside of the square root
+            double insideSquareRoot = (b * b) - 4 * a * c;
+
+            if (insideSquareRoot < 0)
+            {
+                //There is no solution
+                x1 = double.NaN;
+                x2 = double.NaN;
+            }
+            else
+            {
+                //Compute the value of each x
+                //if there is only one solution, both x's will be the same
+                double sqrt = Math.Sqrt(insideSquareRoot);
+                x1 = (-b + sqrt) / (2 * a);
+                x2 = (-b - sqrt) / (2 * a);
+            }
         }
+        */
+
+
     }
 }
