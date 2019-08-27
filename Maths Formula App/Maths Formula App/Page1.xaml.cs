f757178bcc6e4a5e6f6a7de70dcffb425fcba7e2
James Low <1012667@student.ccgs.wa.edu.au>
2019-08-27 09:16:05 +0800

    Added Maths Formula App/Maths Formula App

--- Maths Formula App/Maths Formula App/Page1.xaml.cs
+++ Maths Formula App/Maths Formula App/Page1.xaml.cs
@@ -225,7 +225,8 @@ namespace Maths_Formula_App
 
         private void help_Click(object sender, RoutedEventArgs e)
         {
-
+            Frame.Navigate(new Help());
+            this.Frame.Navigate(new Uri("Help.xaml", UriKind.Relative));
         }
     }
 }
