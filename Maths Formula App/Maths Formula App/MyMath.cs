f3367b27aeee69a5a791198866e99067c33b8c54
Parker Robinson <1016834@student.ccgs.wa.edu.au>
2019-09-03 14:51:35 +0800

    calculator additon testing

---
+++ Maths Formula App/Maths Formula App/MyMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maths_Formula_App
+{
+    public class MyMath
+    {
+        public double a { get; set; }
+
+        public int b { get; set; }
+
+        public double c { get; set; }
+
+        public string formula { get; set; }
+
+        public double result { get; set; }
+
+    }
+}
