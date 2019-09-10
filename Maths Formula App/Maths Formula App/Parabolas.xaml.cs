3e1caa17240b93c0a1e3ab8ed86b53b6bd753010
Matthieu Cobby Gagnon <1020709@student.ccgs.wa.edu.au>
2019-09-10 09:28:38 +0800

    making parabola

--- Maths Formula App/Maths Formula App/Parabolas.xaml.cs
+++ Maths Formula App/Maths Formula App/Parabolas.xaml.cs
@@ -25,6 +25,41 @@ namespace Maths_Formula_App
             InitializeComponent();
         }
 
-        
+        private void buttonClicked(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(___inputBoxa__.Text);
+            double b = Convert.ToDouble(___inputBoxh__.Text);
+            double c = Convert.ToDouble(___inputBoxk__.Text);
+
+            double origin_x = ___theCanvas_.Width / 2;
+            double origin_y = ___theCanvas_.Height / 2;
+
+            for (var i = origin_x; i <= ___theCanvas_.Width; i += 1)
+            {
+                double y = a * i + b * i + c;
+                //draw a circle
+            }
+
+
+            //List<int> nums = new List<int>();
+
+                //for (var i = 0; i <= ___theCanvas_.Width; i += 1)
+                //{
+                //    nums.Add(i);
+                //}
+
+
+
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void help_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
