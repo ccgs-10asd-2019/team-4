21a209a863ec1a462688f94e4ac8a707a8bcd6fa
Matthieu Cobby Gagnon <1020709@student.ccgs.wa.edu.au>
2019-09-18 13:28:09 +0800

    Enter a commit message <required>
    added logs

---
+++ Maths Formula App/Maths Formula App/logarithms.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Maths_Formula_App
+{
+    /// <summary>
+    /// Interaction logic for logarithms.xaml
+    /// </summary>
+    public partial class logarithms : Page
+    {
+        public logarithms()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClicked(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(___inputBoxa__.Text);
+            double b = Convert.ToDouble(___inputBoxb__.Text);
+            double c = Convert.ToDouble(___inputBoxc__.Text);
+
+            double power = Math.Log(a, b);
+
+            ___endProductBox_.Text = Convert.ToString(power);
+
+        }
+    }
+}
